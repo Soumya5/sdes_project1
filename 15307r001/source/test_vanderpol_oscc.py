62adb7db60a6559ee8d754a3cced12f4713ece30
Soumya5 <duttasoumya6@gmail.com>
2016-10-22 22:03:38 +0530

    PEP8 fixes

--- 15307r001/source/test_vanderpol_oscc.py
+++ 15307r001/source/test_vanderpol_oscc.py
@@ -1,26 +1,26 @@
 from vanderpol_oscc import function as f
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.integrate import odeint 
+from scipy.integrate import odeint
 import unittest
+
 
 class TestProject1(unittest.TestCase):
 
     """
     test_function tests whether function is correctly written
-    test_soln tests whether solution of the ODE is coming out to be correct 
+    test_soln tests whether solution of the ODE is coming out to be correct
     """
     def test_function(self):
-        sol = f([0,0],100)
-        self.assertAlmostEqual(sol[0],0.0)
-        self.assertAlmostEqual(sol[1],0.0)
+        sol = f([0, 0], 100)
+        self.assertAlmostEqual(sol[0], 0.0)
+        self.assertAlmostEqual(sol[1], 0.0)
 
     def test_soln(self):
         t = np.linspace(0, 1, 100)
-        sol = odeint(f,[0,0],t)
+        sol = odeint(f, [0, 0], t)
         for i in range(100):
-            self.assertAlmostEqual(sol[i,0],0.0)
-            self.assertAlmostEqual(sol[i,1],0.0)
+            self.assertAlmostEqual(sol[i, 0], 0.0)
+            self.assertAlmostEqual(sol[i, 1], 0.0)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
--- 15307r001/source/vanderpol_oscc.py
+++ 15307r001/source/vanderpol_oscc.py
@@ -2,59 +2,63 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 import numpy as np
 
-#This mu needs to be changed for changing the parameter
-mu=5
+# This mu needs to be changed for changing the parameter
+mu = 5
 
-#For changing the initial conditions please change init_cond: init_cond[0]=x(t), init_cond[1]=dx(t)/dt
-init_cond=[0,-1]
+''' For changing the initial conditions please change init_cond
+    init_cond[0]=x(t),
+    init_cond[1]=dx(t)/dt
+'''
+init_cond = [0, -1]
 
-def function(init_cond,t):
+
+def function(init_cond, t):
 
     '''
     This function returns the differential equations using two states
     the state x(t) and its derivative
     '''
-    x1=init_cond[1]
-    x2=mu*(1-(init_cond[0]**2))*init_cond[1]-init_cond[0]
-    f=[x1,x2]
+    x1 = init_cond[1]
+    x2 = mu*(1-(init_cond[0]**2))*init_cond[1]-init_cond[0]
+    f = [x1, x2]
     return f
-    
-def state_plot(sol,t):
+
+
+def state_plot(sol, t):
 
     '''
-    This function creates the plot of the two states-x(t) and dx(t)/dt vs time. 
+    This function creates the plot of the two states-x(t) and dx(t)/dt vs time.
     Different options for customizing the plot features are also provided in
     this function.
     '''
-    fig=plt.figure(1)
-    plt.clf()
-    line1, = plt.plot(t, sol[:,0], '.-', label='x', linewidth=1,
-                      markersize=1, markeredgewidth=1,color='r')
-    line2, = plt.plot(t, sol[:,1], 'D-', label='dx/dt', linewidth=2,
-                      markersize=1, markeredgewidth=1,color='b')
+    line1, = plt.plot(t, sol[:, 0], '.-', label='x', linewidth=1,
+                      markersize=1, markeredgewidth=1, color='r')
+    line2, = plt.plot(t, sol[:, 1], 'D-', label='dx/dt', linewidth=2,
+                      markersize=1, markeredgewidth=1, color='b')
     plt.grid()
-    plt.title("Plot of x(t) and dx(t)/dt vs Time with mu= "+str(mu)+"roll 15307r001",fontname='serif',fontsize=14)
-    plt.xlabel("Time",fontname='serif',fontsize=12)
+    plt.title("Plot of x(t) and dx(t)/dt vs Time with mu= "+str(mu) +
+              "roll 15307r001", fontname='serif', fontsize=14)
+    plt.xlabel("Time", fontname='serif', fontsize=12)
     plt.xticks(range(0, 50)[::2], family='serif', fontsize=8)
-    plt.ylabel("States",fontname='serif',fontsize=12)
+    plt.ylabel("States", fontname='serif', fontsize=12)
     plt.yticks(family='serif', fontsize=12)
     plt.legend(prop={'family': 'serif', 'size': 12})
     plt.savefig('Plot_of_states.png')
-    
+
+
 def phase_plot(sol):
 
     '''
     This function creates the phase plot of the oscillator for a particular
     value of mu
     '''
-    fig=plt.figure(2)
-    plt.clf() 
     plt.grid()
-    plt.plot(sol[:,0], sol[:,1], linewidth=2, label='Phase Plot')
-    plt.title('Phase Plot with mu= '+str(mu)+"roll 15307r001", fontname='serif', fontsize=14)
+    plt.plot(sol[:, 0], sol[:, 1], linewidth=2, label='Phase Plot')
+    plt.title('Phase Plot with mu= '+str(mu) +
+              "roll 15307r001", fontname='serif', fontsize=14)
     plt.xlabel('x', fontname='serif', fontsize=12)
     plt.ylabel('dx/dt', fontname='serif', fontsize=12)
-    plt.savefig('Phase_plot.png')  
+    plt.savefig('Phase_plot.png')
 
 
 def main():
@@ -62,12 +66,10 @@ def main():
     '''
     solves the differential equation and creates the two required plots
     '''
-    t=np.linspace(0,50,10000)
-    sol=odeint(function,init_cond,t)
-    state_plot(sol,t)
+    t = np.linspace(0, 50, 10000)
+    sol = odeint(function, init_cond, t)
+    state_plot(sol, t)
     phase_plot(sol)
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     main()
-
-
